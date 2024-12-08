79fe559624e570303e67a09a5a5d81cf4015dfe8
AI研究人 <y.takak1234@gmail.com>
2024-12-08 17:57:53 +0900

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
-
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+title Stable Diffusion Web UI Japanese - ようこそ
+echo Stable Diffusionへようこそ！このアプリは、AUTOMATIC1111さんのものを改造した、AIで画像を生成したり編集したりするソフトです。ソフト自体は英語です。任意のキーを押すとセットアップを開始します。
+pause > nul
+title Stable Diffusion Web UI Japanese - 変数の確認
+set COMMANDLINE_ARGS=--use-cpu all --precision full --no-half --skip-torch-cuda-test
+set SD_WEBUI_RESTART=tmp/restart
+set ERROR_REPORTING=FALSE
 
 call webui.bat
